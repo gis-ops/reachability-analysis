eac93adeeece1a4e6d963df4c555b918142203c4
TimMcCauley <timothy@openrouteservice.org>
2019-01-27 15:26:17 +0100

    Updates

--- precache-manifest.bc0e3addc30cdafa8583d5f293640648.js
+++ precache-manifest.bc0e3addc30cdafa8583d5f293640648.js
@@ -64,10 +64,6 @@ self.__precacheManifest = [
     "url": "/reachability-analysis/static/media/brand-icons.a046592b.woff"
   },
   {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/reachability-analysis/static/media/icons.8e3c7f55.eot"
-  },
-  {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/reachability-analysis/static/media/icons.b87b9ba5.ttf"
   },
@@ -78,6 +74,10 @@ self.__precacheManifest = [
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
     "url": "/reachability-analysis/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/reachability-analysis/static/media/icons.8e3c7f55.eot"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reachability-analysis/precache-manifest.ee53315f8b02989e3782fe714473dd1e.js"
+  "/reachability-analysis/precache-manifest.bc0e3addc30cdafa8583d5f293640648.js"
 );
 
 workbox.clientsClaim();
